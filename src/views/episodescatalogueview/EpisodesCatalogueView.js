e509ec57189deb48edc409c88dd44c8e8f3c1741
Karl Sjöstrand <karljsjo@gmail.com>
2021-05-13 17:33:48 +0200

    sending a state with useHistory and useLocation from view to view

--- src/views/episodescatalogueview/EpisodesCatalogueView.js
+++ src/views/episodescatalogueview/EpisodesCatalogueView.js
@@ -9,13 +9,13 @@ export const EpisodesCatalogueView = () => {
     <div>
       <h1>Select an episode to view</h1>
       <h2>Current</h2>
-      <button onClick={() => history.push(RoutingPaths.episodeView, 'Current')}>Current</button>
+      <button onClick={() => history.push(RoutingPaths.episodeView, { header: 'Current Episode' })}>Current</button>
       <h2>Random</h2>
-      <button>Random</button>
+      <button onClick={() => history.push(RoutingPaths.episodeView, { header: 'Current Episode' })}>Random</button>
       <h2>Previews</h2>
-      <button>P1</button>
-      <button>P2</button>
-      <button>P3</button>
+      <button onClick={() => history.push(RoutingPaths.episodeView, { header: 'Current Episode' })}>P1</button>
+      <button onClick={() => history.push(RoutingPaths.episodeView, { header: 'Current Episode' })}>P2</button>
+      <button onClick={() => history.push(RoutingPaths.episodeView, { header: 'Current Episode' })}>P3</button>
     </div>
   )
 }
--- src/views/episodeview/EpisodeView.js
+++ src/views/episodeview/EpisodeView.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useEffect } from 'react'
 import { useHistory, useLocation } from "react-router-dom";
 import RoutingPaths from '../../routes/RoutingPaths'
 
@@ -6,13 +7,23 @@ export const EpisodeView = () => {
   const history = useHistory()
   const location = useLocation()
 
+  useEffect(() => {
+    console.log(location.state)
+  }, [location.state])
+
   return (
     <div>
       <button onClick={() => history.push(RoutingPaths.episodesCatalogueView)}>Catalogue</button>
-      <h1>Episode</h1>
-      <h2>⏮ ⏪</h2>
+      <h1>{location.state.header}</h1>
+      <h2>
+        ⏮ 
+        ⏪
+      </h2>
       <h2>..img..</h2>
-      <h2>⏩ ⏭</h2>
+      <h2>
+        ⏩ 
+        ⏭
+      </h2>
     </div>
   )
 }
