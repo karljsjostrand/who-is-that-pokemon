7bbda6e58c90d911439e8abcab87290b1e89c7c9
Karl Sjöstrand <karljsjo@gmail.com>
2021-05-14 18:49:15 +0200

    added loading spinner for when fetching pokemon

--- src/views/pokemonview/PokemonView.js
+++ src/views/pokemonview/PokemonView.js
@@ -2,6 +2,7 @@ import './PokemonView.css'
 import React from 'react'
 import { useEffect, useState } from 'react'
 import { useHistory, useLocation } from "react-router-dom";
+import PokeAPIService from '../../shared/api/service/PokeAPIService'
 import RoutingPaths from '../../routes/RoutingPaths'
 import WhosThatPokemonImage from '../../shared/resources/images/whos-that-pokemon.bmp'
 
@@ -11,6 +12,7 @@ export const PokemonView = () => {
   const [answer, setAnswer] = useState('')
   const [isCorrectAnswer, setIsCorrectAnswer] = useState(false)
   const [pokemon, setPokemon] = useState()
+
 
   useEffect(() => {
     setPokemon(location.state.pokemon)
@@ -22,6 +24,14 @@ export const PokemonView = () => {
 
   const fetchAbilities = () => {
 
+  }
+
+  const fetchAbility = async (nameOrId) => {
+    const { data } = await PokeAPIService.getAbility(nameOrId)
+    data.effect_entries.forEach(element => {
+      if (element.language.name === 'en')
+        return element.effect
+    });
   }
 
   // Returns string with the first letter to upper case. Combined names have each - separated part capitalized.
@@ -46,26 +56,27 @@ export const PokemonView = () => {
         </div>
   }
 
-  const displayAbilities = () => {
-
-    return pokemon?.abilities?.map((abi, i) => <div key={i}>
-      <h3>{capitalizeName(abi.ability.name)}</h3>
-      <h4>ability text</h4>
-    </div>
-    )
-  }
-
   const displayPokemon = () => {
     return <div>
       <h1>It's {capitalizeName(pokemon?.name)}!</h1>
+      <img src={pokemon?.sprites?.front_default} alt='pokemon sprite' />
       <h2>Abilities</h2>
       {displayAbilities()}
-      <img src={pokemon?.sprites?.front_default} alt='pokemon sprite' />
     </div>
   }
 
+  const displayAbilities = () => {
+    return pokemon?.abilities?.map((abi, i) => <div key={i}>
+      <h3>{capitalizeName(abi.ability.name)}</h3>
+      {/* {abi.ability = [...abi.ability, ]} */}
+      {/* {fetchAbility(abi.ability.name)} */}
+      <h4>{abi.ability.url}</h4>
+    </div>
+    )
+  }
+  
   return (
-    <div>
+    <div className='pokemon-container'>
       {displayAnswer()}
       {displayPokemon()}
       <img className='whos-that-pokemon' src={WhosThatPokemonImage} alt='whos that pokemon?' onClick={() => history.push(RoutingPaths.whosThatPokemonView)} />
--- src/views/whosthatpokemonview/WhosThatPokemonView.js
+++ src/views/whosthatpokemonview/WhosThatPokemonView.js
@@ -4,29 +4,13 @@ import { useHistory } from 'react-router-dom'
 import RoutingPaths from '../../routes/RoutingPaths'
 import PokeAPIService from '../../shared/api/service/PokeAPIService'
 import WhosThatPokemonImage from '../../shared/resources/images/whos-that-pokemon.bmp'
-import PokeAPI from '../../shared/api/PokeAPI'
+import PokeballImage from '../../shared/resources/images/pokeball.png'
 
 export const WhosThatPokemonView = () => {
   const history = useHistory()
   const [pokemon, setPokemon] = useState()
   const [answer, setAnswer] = useState('')
-
-  const fetchRandomPokemon = async () => {
-    const response = await PokeAPIService.getAllPokemon()
-
-    console.log(response.data)
-    console.log(response.data.count)
-
-    const max = response.data.count
-    const rnd = getRandom(max)
-
-    const randomPokemonName = response.data.results[rnd].name
-    console.log(randomPokemonName)
-
-    const { data } = await PokeAPIService.getPokemon(randomPokemonName)
-    console.log(data)
-    setPokemon(data)
-  }
+  const [isLoading, setIsLoading] = useState(true)
 
   // Returns a random integer between 0 and max.
   const getRandom = (max) => {
@@ -35,6 +19,20 @@ export const WhosThatPokemonView = () => {
   }
 
   useEffect(() => {
+    const fetchRandomPokemon = async () => {
+      const response = await PokeAPIService.getAllPokemon()
+  
+      const max = response.data.count
+      const rnd = getRandom(max)
+  
+      const randomPokemonName = response.data.results[rnd].name
+      console.log(randomPokemonName)
+  
+      const { data } = await PokeAPIService.getPokemon(randomPokemonName)
+      setPokemon(data)
+      setIsLoading(false)
+    }
+
     fetchRandomPokemon()
   }, [])
 
@@ -46,18 +44,30 @@ export const WhosThatPokemonView = () => {
   return (
     <div className='whos-that-pokemon-container'>
       <div className='secret-pokemon'>
-        <img className='who-is-it' src={WhosThatPokemonImage} alt={'whos that pokemon?'} onClick={() => revealPokemon()} />
+        <img 
+          className='who-is-it' 
+          src={WhosThatPokemonImage} 
+          alt={'whos that pokemon?'} 
+          onClick={() => revealPokemon()} />
         {/* load back sprite, if back is missing load front */}
-        <img className='secret-pokemon' src={pokemon?.sprites?.back_default ? pokemon?.sprites?.back_default : pokemon?.sprites?.front_default} alt='pokemon sprite error' onClick={() => revealPokemon()} />
+        <img 
+          className={isLoading ? 'secret-pokemon loading ' : 'secret-pokemon'}
+          src={isLoading ? PokeballImage 
+                         : pokemon?.sprites?.back_default ? pokemon?.sprites?.back_default : pokemon?.sprites?.front_default} 
+          alt='pokemon sprite error' 
+          onClick={() => revealPokemon()} />
       </div>
       <div className='input-answer'>
         <form onSubmit={() => revealPokemon()}>
-          <input className='input-answer' placeholder='WHO&apos;S THAT POKEMON?' onChange={(event) => setAnswer(event.target.value.toLowerCase())}></input>
+          <input 
+            className='input-answer' 
+            placeholder='WHO&apos;S THAT POKEMON?' 
+            onChange={(event) => setAnswer(event.target.value.toLowerCase())}></input>
         </form>
       </div>
-      <div>
+      {/* <div>
         <iframe className='youtube-embed' width="280" height="157" src="https://www.youtube-nocookie.com/embed/gOLXYAlC-R8" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-      </div>
+      </div> */}
     </div>
   )
 }
